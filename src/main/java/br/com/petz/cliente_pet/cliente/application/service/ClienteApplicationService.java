47f0f6fbb1ba556adc44f4956a756082ce37c184
Roberto Pedro <robertopedro100@gmail.com>
2025-03-27 12:47:57 -0300

    Implementa ClienteApplicationService e corrige ClienteAPI

---
+++ src/main/java/br/com/petz/cliente_pet/cliente/application/service/ClienteApplicationService.java
@@ -0,0 +1,20 @@
+package br.com.petz.cliente_pet.cliente.application.service;
+
+import br.com.petz.cliente_pet.cliente.application.api.ClienteRequest;
+import br.com.petz.cliente_pet.cliente.application.api.ClienteResponse;
+import br.com.petz.cliente_pet.cliente.domain.Cliente;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Service;
+
+@Service
+@Log4j2
+public class ClienteApplicationService implements ClienteService {
+
+    @Override
+    public ClienteResponse criaCliente(ClienteRequest clienteRequest) {
+        log.info("[inicia] ClienteApplicationService - criaCliente");
+        Cliente cliente = clienteRepository.salva(new Cliente(clienteRequest));
+        log.info("[finaliza] ClienteApplicationService - criaCliente");
+        return null;
+    }
+}
