392580668248c29c5c3f747632a0c14ce0799220
Roberto Pedro <robertopedro100@gmail.com>
2025-03-31 19:28:29 -0300

    implementa fluxo API, teste ok!

--- src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteAPI.java
+++ src/main/java/br/com/petz/cliente_pet/cliente/application/api/ClienteAPI.java
@@ -4,6 +4,8 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/v1/cliente")
 public interface ClienteAPI {
@@ -11,4 +13,8 @@ public interface ClienteAPI {
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
     ClienteResponse postCliente(@Valid @RequestBody ClienteRequest clienteRequest);
+
+    @GetMapping
+    @ResponseStatus(code = HttpStatus.OK)
+    List<ClienteListResponse> getTodosClientes();
 }
